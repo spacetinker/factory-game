07c8fb412be2682b79b86dc952522ca790281ec8
spacetinker <davidrobertmorea@gmail.com>
2025-03-18 12:37:56 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-
+function closeintro() {
+  document.getElementbById("intro").hidden = true;
+  document.getElementbById("buttons").hidden = false;
+}
+function productionmenu() {
+  document.getElementById("produce").hidden = false;
+}
