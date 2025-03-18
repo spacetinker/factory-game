7bfd18b4ee693bc23dfac0d6dabfefa2b68d67dd
spacetinker <davidrobertmorea@gmail.com>
2025-03-18 13:15:17 -0500

    Update main.js

--- main.js
+++ main.js
@@ -3,5 +3,50 @@ function closeintro() {
   document.getElementbById("buttons").hidden = false;
 }
 function productionmenu() {
-  document.getElementById("produce").hidden = false;
+  document.getElementById("production").hidden = false;
+  document.getElementById("research").hidden = true;
+  document.getElementById("administration").hidden = true;  
+  document.getElementById("stockpile").hidden = true;
+  document.getElementById("contracts").hidden = true;
+  document.getElementById("statistics").hidden = true;
+}
+function stockmenu() {
+  document.getElementById("production").hidden = true;
+  document.getElementById("research").hidden = true;
+  document.getElementById("administration").hidden = true;  
+  document.getElementById("stockpile").hidden = false;
+  document.getElementById("contracts").hidden = true;
+  document.getElementById("statistics").hidden = true;
+}
+function adminmenu() {
+  document.getElementById("production").hidden = true;
+  document.getElementById("research").hidden = true;
+  document.getElementById("administration").hidden = false;  
+  document.getElementById("stockpile").hidden = true;
+  document.getElementById("contracts").hidden = true;
+  document.getElementById("statistics").hidden = true;
+}
+function researchmenu() {
+  document.getElementById("production").hidden = true;
+  document.getElementById("research").hidden = false;
+  document.getElementById("administration").hidden = true;  
+  document.getElementById("stockpile").hidden = true;
+  document.getElementById("contracts").hidden = true;
+  document.getElementById("statistics").hidden = true;
+}
+function statsmenu() {
+  document.getElementById("production").hidden = true;
+  document.getElementById("research").hidden = true;
+  document.getElementById("administration").hidden = true;  
+  document.getElementById("stockpile").hidden = true;
+  document.getElementById("contracts").hidden = true;
+  document.getElementById("statistics").hidden = false;
+}
+function contracts() {
+  document.getElementById("production").hidden = true;
+  document.getElementById("research").hidden = true;
+  document.getElementById("administration").hidden = true;  
+  document.getElementById("stockpile").hidden = true;
+  document.getElementById("contracts").hidden = true;
+  document.getElementById("statistics").hidden = false;
 }
